3a6085c66fe6045a3c5028225ab13c9135238c10
Gabit-2007 <gabilovemel@gmail.com>
2025-10-07 10:17:00 -0300

    novas modificações

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -9,18 +9,18 @@ export default function Inicio(){
         <Cabecalho />
         <div className='main'>
         <p>Seja bem-vindo à nossa livraria! Aqui, a paixão pela leitura ganha vida. Nossa missão é espalhar o amor pelos livros e criar um espaço acolhedor para todos os amantes da literatura. Conheça nossa equipe dedicada e fique por dentro dos eventos emocionantes que promovemos. Venha explorar um mundo de histórias e conhecimento!</p>
-        </div>
         <Cartao
-        imagem="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.metropoles.com%2Fzip%2Fpor-que-historias-com-muitos-super-herois-sao-uma-droga&psig=AOvVaw3DgrdsbgHrmGKnxCIImAB6&ust=1759872314760000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMC69K3BkJADFQAAAAAdAAAAABAE"
+        imagem="https://fly.metroimg.com/upload/q_85,w_700/https://uploads.metroimg.com/wp-content/uploads/2017/11/21151223/classic-justice-league.jpg"
         titulo="Livros de super heróis"
         descricao="Em nossa livraria você vai encontrar livros de diversos heróis da atualidade!"
         />
         <Cartao
-        imagem="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.metropoles.com%2Fzip%2Fpor-que-historias-com-muitos-super-herois-sao-uma-droga&psig=AOvVaw3DgrdsbgHrmGKnxCIImAB6&ust=1759872314760000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMC69K3BkJADFQAAAAAdAAAAABAE"
-        titulo="Livros de super heróis"
-        descricao="Em nossa livraria você vai encontrar livros de diversos heróis da atualidade!"
+        imagem="https://gastronomiacarioca.zonasul.com.br/wp-content/uploads/2022/03/receitas_chocolate_zona_sul_destaque_gotas_brownie.jpg"
+        titulo="Livros de receitas"
+        descricao="Se quer cozinhar algo gostoso, compre livros de receitas com tudo que precisa saber!"
         inverter
         />
+        </div>
         <Rodape />
         </div>
     );
